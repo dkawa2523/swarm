9ac1b0c65d617f5b613cdd87aebdd0b9cab16b07
dkawa2523 <68120833+dkawa2523@users.noreply.github.com>
2026-04-26 14:34:16 +0900

    Add shared grid package exports

---
+++ electron_swarm/grids/__init__.py
@@ -0,0 +1,5 @@
+"""Shared grid construction helpers."""
+
+from .energy import EnergyGrid, build_energy_grid, refine_with_thresholds
+
+__all__ = ["EnergyGrid", "build_energy_grid", "refine_with_thresholds"]
